f492e6d2569fe62d9b793defa313fd0ad766077e
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-18 17:45:50 +0530

    Add files via upload

---
+++ Bs5.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Bs5{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+       int c=sc.nextInt();
+       if(a>=b&&a>=c)
+        System.out.print(a);
+       else if(b>=a&&b>=c)
+       System.out.print(b);
+       else
+       System.out.print(c);
+    }
+}
