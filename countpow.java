23fa6e51e2417190653fbdc30b61088ac53bd41e
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-18 18:23:54 +0530

    Add files via upload

---
+++ countpow.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.math.*;
+class countpow{
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int s=sc.nextInt();
+		int n=sc.nextInt();
+	    int a=(int)Math.pow(s,n);
+	    System.out.print(a);
+		
+	}
+}
