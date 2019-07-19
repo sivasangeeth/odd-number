f87e338b3ba145561f990ce75bd3ce939254b4d0
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-19 18:10:48 +0530

    Add files via upload

---
+++ mu.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class mu {
+   public static void main(String args[]){
+      Scanner sc = new Scanner(System.in);
+      int num1 = sc.nextInt();
+    //  int fact=0;
+      for(int i=1;i<=5;i++)
+      {
+      System.out.print(i*num1+" ");
+   }}}
