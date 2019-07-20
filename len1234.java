22e3fbdc1b62a8a8a36c631e23c2df1b13d3317c
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-20 11:08:41 +0530

    Add files via upload

---
+++ len1234.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.math.*;
+class len1234{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String n=sc.nextLine();
+        String s[]=n.split("\\s");
+        System.out.print(s.length-1);
+    }    
+}
+
