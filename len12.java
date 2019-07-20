c8429eb33fbabdab799d1f2f263263b006e9440e
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-20 10:59:34 +0530

    Add files via upload

---
+++ len12.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.math.*;
+class len12{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String n=sc.nextLine();
+        String s=n.replaceAll(" ","");
+        System.out.print(s.length());
+    }
+        
+}
+
