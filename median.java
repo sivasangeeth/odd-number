f41584fcf35b8771e818cee861bd26849a2e856f
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-20 09:45:06 +0530

    Add files via upload

---
+++ median.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class median{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        Arrays.sort(a);
+        int len=a.length/2;
+        System.out.println(a[len]);
+    }
+}
