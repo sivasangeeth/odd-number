c8441f78b9f8f7806de00d0bcdf9fc2f2c55b208
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-20 11:24:12 +0530

    Add files via upload

---
+++ swap12.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class swap12{
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+	    int n1=sc.nextInt();
+	    int c=n;
+	    n=n1;
+	    n1=c;
+	    System.out.print(n+" "+n1);
+	}
+}
+
+
