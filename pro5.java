2a53f46bbf6b1c0992271fda4608997e7916d191
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-22 13:52:14 +0530

    Add files via upload

---
+++ pro5.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class pro5{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+      //  int s=sc.nextInt();
+       String s=sc.nextLine();
+       int n=s.length();
+       System.out.print(n);
+    }
+}
+
+
