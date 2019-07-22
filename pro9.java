184f1e270799b636795e85d7e272d57560544d10
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-22 14:46:29 +0530

    Add files via upload

---
+++ pro9.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class pro9{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       int a1=sc.nextInt();
+       int sum=a+a1;
+       System.out.println(sum);
+   
+   }
+}
+
+
