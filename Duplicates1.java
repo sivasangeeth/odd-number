fd4b068768a7951c9c4ba180564e22ea97acfdc5
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-22 19:05:27 +0530

    Add files via upload

---
+++ Duplicates1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Duplicates1{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+       System.out.println(s.length());
+    }
+}
+
+
+
+
