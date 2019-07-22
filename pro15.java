1e08adc1599b8207d35bc6869b0bfcac3153572c
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-22 15:33:44 +0530

    Add files via upload

---
+++ pro15.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class pro15{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int n2=sc.nextInt();
+        int n1=n*n2;
+        if(n1%2!=0)
+       System.out.print("odd");
+       else
+          System.out.print("even");
+       
+   }
+}
+
+
+
