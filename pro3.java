450f622b37b829dfe7618b5ece00f89bbed736ee
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-22 12:29:55 +0530

    Add files via upload

---
+++ pro3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class pro3{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+         String s1=sc.nextLine();
+         
+       System.out.print(s+s1);
+    }
+}
+
