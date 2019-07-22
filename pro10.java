6e773c1642b87b6ba05e9c0eb9d4187105fb3581
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-22 14:50:42 +0530

    Add files via upload

---
+++ pro10.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class pro10{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       if(a%2==0)
+       System.out.println("yes");
+       else
+        System.out.println("no");
+   
+   }
+}
+
+
