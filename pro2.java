90a5883d1a93bc5c48f6c53ca1e381d1e5675b14
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-22 12:26:01 +0530

    Add files via upload

---
+++ pro2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class pro2{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+         String s1=sc.nextLine();
+       int s2=s.length();
+       int s3=s1.length();
+       if(s2>s3)
+       System.out.print(s);
+       else
+       System.out.print(s1);
+    }
+}
+
