13e2beffc4e6e02dd2cd87607710f4320daed597
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-22 14:54:56 +0530

    Add files via upload

---
+++ pro11.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class pro11{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        for(int i=0;i<a.length();i++)      
+        {
+        System.out.print(a.charAt(i)+" ");
+        }
+   }
+}
+
+
