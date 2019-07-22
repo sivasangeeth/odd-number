d48415487d3555de986a30416f404e8d3651012e
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-22 15:12:23 +0530

    Add files via upload

---
+++ pro12.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class pro12{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n==1)
+        System.out.print("One");
+        else if(n==2)
+        System.out.print("Two");
+        else if(n==3)
+        System.out.print("Three");
+        else if(n==4)
+        System.out.print("Four");
+        else if(n==5)
+        System.out.print("Five");
+        else if(n==6)
+        System.out.print("Six");
+        else if(n==7)
+        System.out.print("Seven");
+        else if(n==8)
+        System.out.print("Eight");
+        else if(n==9)
+        System.out.print("Nine");
+        else
+        System.out.print("Ten");
+   }
+}
+
+
