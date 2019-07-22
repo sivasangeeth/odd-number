0f814ba1aa9c65a53e9e04313e5600a70d11b7e4
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-22 14:43:08 +0530

    Add files via upload

---
+++ pro8.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class pro8{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+       int a[]=new int[s];
+       int sum=0;
+       for(int i=0;i<s;i++)
+       {
+           a[i]=sc.nextInt();
+       }
+       for(int i=0;i<s;i++)
+       {
+           sum+=a[i];
+       }
+       System.out.println(sum/s);
+   
+   }
+}
+
+
