ff24ec0c8d04b75e5103dbd21dfdd0b9dd1af223
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-24 10:05:24 +0530

    Add files via upload

---
+++ ad1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class ad1{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=a+b;
+        if(c%2==0)
+        System.out.print("even");
+        else
+        System.out.print("odd");
+        
+              }
+}
+
+
+
+
