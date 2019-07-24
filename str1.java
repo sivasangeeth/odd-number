1d379d5121b2e4e4ad74052aa5484e612f3b1b2d
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-24 10:17:45 +0530

    Add files via upload

---
+++ str1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class str1{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%2==0)
+        System.out.print("yes");
+        else
+            System.out.print("no");
+              }
+}
+
+
+
+
+
