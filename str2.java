f375636fd2bf7096a023b368173b5e130f23d332
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-24 10:30:08 +0530

    Add files via upload

---
+++ str2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class str2{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int temp=a%10;
+        int tem1=10-temp;
+        System.out.print(a+tem1);
+}}
+
+
+
+
