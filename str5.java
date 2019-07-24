ce7bd97d53bb345e30db70377a06aadf9007f53f
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-24 11:15:50 +0530

    Add files via upload

---
+++ str5.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.math.*;
+class str5{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int s=a+a;
+        //int s=(int)Math.sqrt(a);
+        System.out.print(s);
+}}
+
+
+
+
