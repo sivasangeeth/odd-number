4d49d786a67b45db9ac033251d75a0b8975848c1
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-24 09:37:05 +0530

    Add files via upload

---
+++ substr.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class substr{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String n=sc.nextLine();
+        int n2=sc.nextInt();
+        for(int i=0;i<n2;i++)
+        {
+            System.out.print(n.charAt(i));
+        }
+       
+       
+   }
+}
+
+
+
+
