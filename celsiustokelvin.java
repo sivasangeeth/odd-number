669a1c21e307a7cc0beab3585c56dea40316afbc
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-25 18:06:36 +0530

    Add files via upload

---
+++ celsiustokelvin.java
@@ -0,0 +1,16 @@
+import java.io.*; 
+import java.util.*; 
+class  celsiustokelvin{ 
+
+    public static void main(String[] args) 
+    { 
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=n+273;
+        System.out.print(sum);
+        
+    }
+}
+
+
+
