067555b6a995b10ad81d7440c7afdc0cc9703d74
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-25 11:51:50 +0530

    Add files via upload

---
+++ pro25.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.math.*;
+
+class pro25
+{
+    public static void main(String arv[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int n1=sc.nextInt();
+        int s=n-n1;
+            System.out.print(Math.abs(s));
+    }
+}
+
