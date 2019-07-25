64cd5fbf18f97b45607c0f9e2376780948c221e1
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-25 10:03:29 +0530

    Add files via upload

---
+++ pro18.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.math.*;
+class pro18{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        double n=sc.nextDouble();
+        System.out.print((int)Math.ceil(n));
+    }
+}
