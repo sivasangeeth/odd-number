33890f52c6f95226f45a3d7bbc4e7d91a51a6a1d
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-25 12:19:09 +0530

    Add files via upload

---
+++ pro28.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.math.*;
+
+class pro28
+{
+    public static void main(String arv[])
+    {
+        Scanner sc=new Scanner(System.in);
+       
+        char b=sc.next().charAt(0);
+        int a=b;
+        System.out.print(a);
+    }
+}
+
