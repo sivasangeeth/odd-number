4ac9083f9f3c63e8b3c9508c04831e2f2e366141
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-25 10:34:34 +0530

    Add files via upload

---
+++ pro22.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class pro22
+{
+public static void main(String args[])
+{
+ Scanner in=new Scanner(System.in);
+ int n=in.nextInt();
+   if(n%13==0)
+   System.out.print("yes");
+   else
+   System.out.print("no");
+     
+ }
+}
+
