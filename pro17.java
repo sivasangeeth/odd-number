6e08df32e7a983e0742931f0e7999dcb13048f0c
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-25 09:56:01 +0530

    Add files via upload

---
+++ pro17.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class pro17{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        if(n>n1 && n<n2)
+        System.out.print("yes");
+        else
+        System.out.print("no");
+    }
+}
