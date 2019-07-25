916fb9f26b96271ec4e5dca208f1a3c944b61ce0
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-25 10:31:37 +0530

    Add files via upload

---
+++ pro21.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class pro21
+{
+public static void main(String args[])
+{
+ Scanner in=new Scanner(System.in);
+ int n=in.nextInt();
+ for(int i=1;i<=n;i++ )
+ {
+   if(n%i==0)
+   System.out.print(i+" ");
+     
+ }
+}
+}
