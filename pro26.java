9a272f4e9060d0d23e04fa0df02428c9edf6221f
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-25 11:58:24 +0530

    Add files via upload

---
+++ pro26.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.math.*;
+
+class pro26
+{
+    public static void main(String arv[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int n1=sc.nextInt();
+        float s=n*n1;
+            System.out.printf("%.5f",s);
+    }
+}
+
