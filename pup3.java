6e6633f9fdaaa6102f6b29d85aab374a8f35deda
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-26 11:26:15 +0530

    Add files via upload

---
+++ pup3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.math.*;
+class pup3{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int A=sc.nextInt();
+        int B=sc.nextInt();
+        int C=sc.nextInt();
+        int v=(A*B)/C;
+        System.out.print(v);
+    }
+}
