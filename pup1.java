fb6c8c6716b07f627ba6667cb95055c5daeff80d
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-26 11:09:24 +0530

    Add files via upload

---
+++ pup1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.math.*;
+class pup1{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String n=sc.nextLine();
+        String n1=sc.nextLine();
+        String s=n.concat(n1);
+  //      int s=n^n1;
+         System.out.print(s);
+        
+    }
+}
