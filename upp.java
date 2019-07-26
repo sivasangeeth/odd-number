e5db8a6ca15fdbeb69b698edfbe1f9100ce668e1
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-26 09:52:17 +0530

    Add files via upload

---
+++ upp.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class upp{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+       String s=sc.nextLine();
+       String n1=s.toUpperCase();
+        System.out.print(n1);
+        
+    }
+}
