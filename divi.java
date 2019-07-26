2f8b8ff3db383aee848388e5a6a8186e803ba39c
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-26 09:49:45 +0530

    Add files via upload

---
+++ divi.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class divi{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        int s=n1/2;
+        if(n1%2==0)
+        System.out.print(s);
+        else
+        System.out.print(n1);
+        
+    }
+}
