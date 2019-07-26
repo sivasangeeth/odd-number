ed239750c9c3f997c9e4a36be4c81e09f13acbfc
sivasangeeth <34643270+sivasangeeth@users.noreply.github.com>
2019-07-26 10:47:42 +0530

    Add files via upload

---
+++ pup.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.math.*;
+class pup{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int n1=sc.nextInt();
+        int s=(int)Math.pow(n,n1);
+  //      int s=n^n1;
+         System.out.print(s);
+        
+    }
+}
